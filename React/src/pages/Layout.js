1944b9218067a8d638b737da82934004c18b5c7d
Cactouss <isaline.didier@outlook.com>
2022-01-17 23:00:50 +0100

    lazy loading

--- React/src/pages/Layout.js
+++ React/src/pages/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -6,31 +6,33 @@ import {
 } from 'react-router-dom';
 
 import { Navigation, PrivateRoute } from '../components';
-import Login from './Login';
-import Vendor from './Vendors';
-import Home from './Home';
-import Rewards from './Rewards';
-import Trashs from './Trashs';
-import Users from './Users';
-import PageNotFound from './PageNotFound';
-import Logout from './Logout';
+const Login = React.lazy(()=> import('./Login'));
+const Vendor = React.lazy(()=> import('./Vendors'));
+const Home = React.lazy(()=> import('./Home'));
+const Rewards = React.lazy(()=> import('./Rewards'));
+const Trashs = React.lazy(()=> import('./Trashs'));
+const Users = React.lazy(()=> import('./Users'));
+const PageNotFound = React.lazy(()=> import('./PageNotFound'));
+const Logout = React.lazy(()=> import('./Logout'));
 
 export default function Layout() {
 	return (
 		<Router>
 			<Navigation />
-			<Routes>
-				<Route element={<PrivateRoute />} >
-					<Route path='/' exact element={<Home />} />
-					<Route path='/Vendors' element={<Vendor />} />
-					<Route path='/Rewards' element={<Rewards />} />
-					<Route path='/Trashs' element={<Trashs />} />
-					<Route path='/Users' element={<Users />} />
-					<Route path='/SignOut' element={<Logout />} />
-				</Route>
-				<Route path='/SignIn' element={<Login />} />
-				<Route path='*' element={<PageNotFound />} />
-			</Routes>
+			<Suspense fallback={<div>Chargement...</div>}>
+				<Routes>
+					<Route element={<PrivateRoute />} >
+						<Route path='/' exact element={<Home />} />
+						<Route path='/Vendors' element={<Vendor />} />
+						<Route path='/Rewards' element={<Rewards />} />
+						<Route path='/Trashs' element={<Trashs />} />
+						<Route path='/Users' element={<Users />} />
+						<Route path='/SignOut' element={<Logout />} />
+					</Route>
+					<Route path='/SignIn' element={<Login />} />
+					<Route path='*' element={<PageNotFound />} />
+				</Routes>
+			</Suspense>
 		</Router >
 	);
 }
